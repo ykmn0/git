fbb3f61c78eb6ec524acd9bff41804111b6dc9d9
ykmn0 <70263406+ykmn0@users.noreply.github.com>
2025-01-07 12:40:06 +0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SERVER_USER="deploy"
+SERVER_IP="84.201.179.236"
+
 DEPLOY_DIR="/var/www/html"
 GIT_REPO_DIR="/tmp/repo"
 
@@ -10,7 +13,7 @@ git clone https://github.com/ykmn0/git.git $GIT_REPO_DIR
 rsync -avz --exclude '.git' $GIT_REPO_DIR/ $DEPLOY_DIR/
 
 echo "Restart Nginx"
-systemctl reload nginx
+ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP 'systemctl reload nginx'
 
 rm -rf $GIT_REPO_DIR
 
