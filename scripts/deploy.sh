941df6d66dea57208c0ead81da953c36f887f022
ykmn0 <70263406+ykmn0@users.noreply.github.com>
2025-01-07 13:48:26 +0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,15 +6,15 @@ REMOTE_PATH="/var/www/html"
 
 LOCAL_PATH="/tmp/repo"
 
-rm -rf $GIT_REPO_DIR
+rm -rf $LOCAL_PATH
 
-git clone https://github.com/ykmn0/git.git $GIT_REPO_DIR
+git clone https://github.com/ykmn0/git.git $LOCAL_PATH
 
 rsync -avz $LOCAL_PATH/index.html $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/
 
 echo "Restart Nginx"
 ssh $REMOTE_USER@$REMOTE_HOST "sudo systemctl restart nginx"
 
-rm -rf $GIT_REPO_DIR
+rm -rf $LOCAL_PATH
 
 echo "Deploy finished"
